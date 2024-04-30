678c58fa62a74456115be370d3a580109b33faf3
Max Dupler <maxdupler@Maxs-MBP.lan>
2024-04-30 13:47:07 -0400

    adding tokenize function

--- HW6/Zpm.cpp
+++ HW6/Zpm.cpp
@@ -3,9 +3,23 @@
 #include <fstream>
 #include <unordered_map>
 #include <any>
+#include <vector>
+#include <sstream>
 
 
 using std::string, std::cout, std::endl, std::cerr;
+
+std::vector<string> tokenize(const string line) {
+    std::vector<string> tokens;
+    std::istringstream sReader(line);
+    string token;
+
+    while(std::getline(sReader, token, ' ')) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
 
 int main(int argc, char* argv[]) {
     // check if arguments are correct
@@ -19,6 +33,17 @@ int main(int argc, char* argv[]) {
     std::ifstream fReader = std::ifstream(fName);
     if (!fReader.good()) {
         cerr << "File does not exist or has an error" << endl;
+    }
+
+    string line;
+    while (std::getline(fReader, line)) {
+        if (!line.empty()) {
+            std::vector<string> tokens = tokenize(line);
+            for (const string token : tokens) {
+                cout << token << " ";
+            }
+        }
+        cout << endl;
     }
     return 0;
 }
